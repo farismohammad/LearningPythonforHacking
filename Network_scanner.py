342c2704e097852d33b9788f8807f4e02152e658
Faris <fmoha@pixelatedspace.me>
2023-09-12 09:50:46 +0400

    Create Network_scanner.py

---
+++ Network_scanner.py
@@ -0,0 +1,29 @@
+import scapy.all as scapy
+import argparse
+ 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP")
+    options = parser.parse_args()
+    return options
+ 
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+ 
+    clients_list = []
+    for element in answered:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwrc}
+        clients_list.append(client_dict)
+    return clients_list
+ 
+def print_result(result_list):
+    print("IP\t\tMAC Address\n--------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["MAC"])
+ 
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
